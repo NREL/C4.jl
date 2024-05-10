36883c203001f9ca623a37b400c03b3637d41a25
Gord Stephen <mail@gord.io>
2024-05-10 12:51:34 -0700

    Add validation to TimeProxyAssignment constructor

--- src/ExpansionModel/time.jl
+++ src/ExpansionModel/time.jl
@@ -1,11 +1,27 @@
 # TODO: Revisit naming. Here we implicitly state that "days" and "TimePeriods"
 #       are synonymous
 # "period" sometimes implies a single timestep, sometimes a contiguous set of timesteps
-# TODO: Enforce valild period:days map and daylength
 struct TimeProxyAssignment
+
     daylength::Int
     periods::Vector{TimePeriod} # set of all TimePeriods
     days::Vector{Int} # mapping from days into TimePeriods set
+
+    function TimeProxyAssignment(periods::Vector{TimePeriod}, days::Vector{Int})
+
+        period_length = length(first(periods))
+        n_periods = length(periods)
+
+        all(period -> length(period) == period_length, periods) ||
+                error("Expected all periods to have the same length")
+
+        all(p -> 1 <= p <= n_periods, days) ||
+                error("Invalid period index in day mapping")
+
+        new(period_length, periods, days)
+
+    end
+
 end
 
 periodcount(tpa::TimeProxyAssignment) = length(tpa.periods)
@@ -17,12 +33,12 @@ end
 function fullchronology(sys::System; daylength::Int=24)
     n_days = daycount(sys, daylength)
     periods = [TimePeriod(((d-1)*daylength+1):(d*daylength), "Day $d") for d in 1:n_days]
-    return TimeProxyAssignment(daylength, periods, collect(1:n_days))
+    return TimeProxyAssignment(periods, collect(1:n_days))
 end
 
 function add_period!(tpa::TimeProxyAssignment, tp::TimePeriod, ds::Vector{Int})
     push!(tpa.periods, tp)
-    days[ds] = tp
+    days[ds] .= tp
     return
 end
 
