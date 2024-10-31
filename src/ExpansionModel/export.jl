198bb13133c73020951d535a133ca44f63f9bada
Gord Stephen <mail@gord.io>
2024-10-31 13:14:59 -0700

    Convert SystemExpansion persistence to DuckDB Appender API

--- src/ExpansionModel/export.jl
+++ src/ExpansionModel/export.jl
@@ -1,5 +1,6 @@
-using DBInterface
-using Dates
+import DBInterface
+import DuckDB
+import Dates: DateTime
 
 import ..store
 
@@ -13,6 +14,24 @@ function store(
     store(con, iter, cem.builds)
     store(con, iter, cem.economicdispatch)
 
+end
+
+struct ExpansionAppender
+
+    sitebuilds::DuckDB.Appender
+    interfacebuilds::DuckDB.Appender
+
+    ExpansionAppender(con::DuckDB.DB) = new(
+        DuckDB.Appender(con, "sitebuilds"),
+        DuckDB.Appender(con, "interfacebuilds")
+    )
+
+end
+
+function DuckDB.close(appender::ExpansionAppender)
+    DuckDB.close(appender.sitebuilds)
+    DuckDB.close(appender.interfacebuilds)
+    return
 end
 
 function store(con::DBInterface.Connection, iter::Int, sys::SystemExpansion)
@@ -37,80 +56,84 @@ function store(con::DBInterface.Connection, iter::Int, sys::SystemExpansion)
         PRIMARY KEY (iteration, region_from, region_to)
     )")
 
+    appender = ExpansionAppender(con)
+
+    foreach(region -> store(appender, iter, region), sys.regions)
+    foreach(iface -> store(appender, iter, iface, sys.regions), sys.interfaces)
+
+    DuckDB.close(appender)
+
     # TODO: Transmission expansion costs
     DBInterface.execute(con, "CREATE VIEW IF NOT EXISTS summary_capex AS
         SELECT iteration, site, tech, region, power * cost_capital_power as cost_power, energy * cost_capital_energy as cost_energy
         FROM sitebuilds JOIN techs USING (tech, region)
     ")
 
-    foreach(region -> store(con, iter, region), sys.regions)
-    foreach(iface -> store(con, iter, iface, sys.regions), sys.interfaces)
+end
+
+function store(appender::ExpansionAppender, iter::Int, region::RegionExpansion)
+
+    foreach(tech -> store(appender, iter, tech, region), region.thermaltechs)
+    foreach(tech -> store(appender, iter, tech, region), region.variabletechs)
+    foreach(tech -> store(appender, iter, tech, region), region.storagetechs)
 
 end
 
-function store(con::DBInterface.Connection, iter::Int, region::RegionExpansion)
-
-    foreach(tech -> store(con, iter, tech, region), region.thermaltechs)
-    foreach(tech -> store(con, iter, tech, region), region.variabletechs)
-    foreach(tech -> store(con, iter, tech, region), region.storagetechs)
-
-end
-
-function store(con::DBInterface.Connection, iter::Int,
+function store(appender::ExpansionAppender, iter::Int,
                tech::TechnologyExpansion, region::RegionExpansion)
 
-    foreach(site -> store(con, iter, site, tech, region), tech.sites)
+    foreach(site -> store(appender, iter, site, tech, region), tech.sites)
 
 end
 
-function store(con::DBInterface.Connection, iter::Int, site::ThermalSiteExpansion,
+function store(appender::ExpansionAppender, iter::Int, site::ThermalSiteExpansion,
                tech::ThermalExpansion, region::RegionExpansion)
 
     total_units = value(site.units_new) + site.params.units_existing 
     total_capacity = total_units * tech.params.unit_size
 
-    DBInterface.execute(
-        con,
-        "INSERT into sitebuilds (
-            iteration, site, tech, region, power
-        ) VALUES (?, ?, ?, ?, ?)",
-        (iter, name(site), name(tech), name(region), total_capacity)
-    )
+    DuckDB.append(appender.sitebuilds, iter)
+    DuckDB.append(appender.sitebuilds, name(site))
+    DuckDB.append(appender.sitebuilds, name(tech))
+    DuckDB.append(appender.sitebuilds, name(region))
+    DuckDB.append(appender.sitebuilds, total_capacity)
+    DuckDB.append(appender.sitebuilds, nothing)
+    DuckDB.end_row(appender.sitebuilds)
 
 end
 
-function store(con::DBInterface.Connection, iter::Int, site::VariableSiteExpansion,
+function store(appender::ExpansionAppender, iter::Int, site::VariableSiteExpansion,
                tech::VariableExpansion, region::RegionExpansion)
 
     total_capacity = value(site.capacity_new) + site.params.capacity_existing
 
-    DBInterface.execute(
-        con,
-        "INSERT into sitebuilds (
-            iteration, site, tech, region, power
-        ) VALUES (?, ?, ?, ?, ?)",
-        (iter, name(site), name(tech), name(region), total_capacity)
-    )
+    DuckDB.append(appender.sitebuilds, iter)
+    DuckDB.append(appender.sitebuilds, name(site))
+    DuckDB.append(appender.sitebuilds, name(tech))
+    DuckDB.append(appender.sitebuilds, name(region))
+    DuckDB.append(appender.sitebuilds, total_capacity)
+    DuckDB.append(appender.sitebuilds, nothing)
+    DuckDB.end_row(appender.sitebuilds)
 
 end
 
-function store(con::DBInterface.Connection, iter::Int, site::StorageSiteExpansion,
+function store(appender::ExpansionAppender, iter::Int, site::StorageSiteExpansion,
                tech::StorageExpansion, region::RegionExpansion)
 
     total_power = value(maxpower(site))
     total_energy = value(maxenergy(site))
 
-    DBInterface.execute(
-        con,
-        "INSERT into sitebuilds (
-            iteration, site, tech, region, power, energy
-        ) VALUES (?, ?, ?, ?, ?, ?)",
-        (iter, name(site), name(tech), name(region), total_power, total_energy)
-    )
+    DuckDB.append(appender.sitebuilds, iter)
+    DuckDB.append(appender.sitebuilds, name(site))
+    DuckDB.append(appender.sitebuilds, name(tech))
+    DuckDB.append(appender.sitebuilds, name(region))
+    DuckDB.append(appender.sitebuilds, total_power)
+    DuckDB.append(appender.sitebuilds, total_energy)
+    DuckDB.end_row(appender.sitebuilds)
 
 end
 
-function store(con::DBInterface.Connection, iter::Int,
+function store(appender::ExpansionAppender, iter::Int,
                iface::InterfaceExpansion, regions::Vector{RegionExpansion})
 
     region_from = name(regions[iface.params.region_from])
@@ -118,12 +141,10 @@ function store(con::DBInterface.Connection, iter::Int,
 
     total_capacity = value(availablecapacity(iface))
 
-    DBInterface.execute(
-        con,
-        "INSERT into interfacebuilds (
-            iteration, region_from, region_to, capacity
-        ) VALUES (?, ?, ?, ?)",
-        (iter, region_from, region_to, total_capacity)
-    )
+    DuckDB.append(appender.interfacebuilds, iter)
+    DuckDB.append(appender.interfacebuilds, region_from)
+    DuckDB.append(appender.interfacebuilds, region_to)
+    DuckDB.append(appender.interfacebuilds, total_capacity)
+    DuckDB.end_row(appender.interfacebuilds)
 
 end
